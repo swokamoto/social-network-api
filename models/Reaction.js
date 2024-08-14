e7098ef98ebe7bad47c41de8accdb41eb0e0f924
swokamoto <swokamoto@gmail.com>
2024-08-13 21:35:40 -0400

    Reaction and Thought models based on requirements

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'You need to provide a reaction!',
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: 'You need to provide a username!'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'You need to provide a thought!',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'You need to provide a username!'
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reaction'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
